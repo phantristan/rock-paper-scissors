b652e8d2c1be4702fd8b934172ee70dafb4a79cd
Tristan <108037650+phantristan@users.noreply.github.com>
2023-06-22 11:59:45 -0500

    Create function to generate random computer choice (rock, paper, or scissors)

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Hello World");
+/* Create function getComputerChoice
+    -Function will randomly return 'Rock', 'Paper'
+     or 'Scissors
+*/
+
+function getComputerChoice() {
+    var choices = ['Rock', 'Paper', 'Scissors'];
+    var randomIndex = Math.floor(Math.random() * choices.length);
+
+    return choices[randomIndex];
+}
